6c74cdaa1d8a611adf967d96d337f6705cfaf157
sabaferdous12 <saba.ferdous.13@ucl.ac.uk>
2016-04-19 18:16:07 +0100

    Added all files on git repo (bin directory)

---
+++ lib/antigenProcessing.pm
@@ -0,0 +1,36 @@
+package antigenProcessing;
+use strict;
+use warnings;
+
+use Exporter qw (import);
+our @EXPORT_OK = qw (
+                        getAntigenChains
+                );
+
+sub getAntigenChains
+{
+    my ($pdbFile) = @_;
+    my (@antigenChainsTemp, @antigenChains, @antigens) ;
+
+    # Grep all antigen chain in PDB file
+    @antigenChainsTemp = `grep "CHAIN A" $pdbFile`;
+
+    # For multiple antigens in PDB file
+    if ( (scalar @antigenChainsTemp) > 1  )
+    {
+        foreach my $ag ( @antigenChainsTemp )
+        {
+            chomp $ag;
+            @antigens = split(m/\s+/, $ag);
+            push (@antigenChains, $antigens[5]);
+        }
+    }
+    # For Single antigen
+    else {
+        @antigens = map {split(" ", $_)} @antigenChainsTemp;
+        push (@antigenChains, $antigens[5]);
+    }
+    
+    return @antigenChains;
+
+}
